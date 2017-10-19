78e339eadcaac346fb2b73b48e25d0a396fc8fb6
Justin He <hejustin@usc.edu>
2017-10-19 13:29:03 -0700

    pushing first /yes route

--- server.js
+++ server.js
@@ -2,17 +2,22 @@
 
 // modules
 var express = require('express');
-var server = express();
-
+var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
-var port = 4200 || process.env.PORT;
+// port
+var port = 8000 || process.env.PORT;
+
+// secrets
+var secrets = require('./secrets.js');
+
+// Required application specific custom router module
+// var routes = require('./src/routes/');
 
 // mongoose connection
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI || )
+mongoose.connect(process.env.MONGODB_URI || secrets.MONGODB_URI)
     .then(() => { // if all is ok we will be here
       console.log('Start');
     })
@@ -21,6 +26,22 @@ mongoose.connect(process.env.MONGODB_URI || )
         process.exit(1);
     });
 
-server.listen(port, function(){
-  console.log('hello world');
+// configuration of libraries
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// routes
+app.route('/yes').get(function(req,res) {
+  res.send("justin is a poo");
+});
+
+app.route('/').get(function(req,res) {
+  console.log("yaaa");
+  res.send("PIA STINKS");
+});
+
+// Spin up the server
+app.listen(port, function() {
+  console.log('running on port', port);
 })
