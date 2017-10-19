4eb902d8ff1b80cca9ecaac9c666dc1d0c604072
Justin He <hejustin@usc.edu>
2017-10-19 12:37:38 -0700

    updating gitignore

--- server.js
+++ server.js
@@ -8,9 +8,18 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var port = 4200;
+var port = 4200 || process.env.PORT;
 
 // mongoose connection
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MONGODB_URI || )
+    .then(() => { // if all is ok we will be here
+      console.log('Start');
+    })
+    .catch(err => { // if error we will be here
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
 
 server.listen(port, function(){
   console.log('hello world');
